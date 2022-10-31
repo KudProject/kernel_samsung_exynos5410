959c923fa2209b2fe93866a59c45c756ce8d4698
Albert I <kras@raphielgang.org>
2022-11-01 07:30:17 +0800

    hid: Fix placement of set_bit() on place that should never be
    The ugly placement means that the bit would never be set at all. Move it
    outside the switch declaration to fix this issue.
    
    Compiler warnings as seen with GCC 7.5.0 (only topmost since everything has the
    exact same root problem):
    
      kernel/samsung/exynos5410/drivers/hid/hid-samsung.c: In function 'samsung_kbd_input_mapping':
      kernel/samsung/exynos5410/drivers/hid/hid-samsung.c:147:21: warning: statement will never be executed [-Wswitch-unreachable]
         set_bit(EV_REP, hi->input->evbit);
                         ~~^~~
      kernel/samsung/exynos5410/arch/arm/include/asm/bitops.h:184:45: note: in definition of macro 'ATOMIC_BITOP'
       #define ATOMIC_BITOP(name,nr,p)  _##name(nr,p)
                                                   ^
      kernel/samsung/exynos5410/drivers/hid/hid-samsung.c:147:3: note: in expansion of macro 'set_bit'
         set_bit(EV_REP, hi->input->evbit);
         ^~~~~~~
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/hid/hid-samsung.c
+++ drivers/hid/hid-samsung.c
@@ -143,8 +143,8 @@ static int samsung_kbd_input_mapping(struct hid_device *hdev,
 		usage->hid & HID_USAGE);
 
 	if (HID_UP_KEYBOARD == (usage->hid & HID_USAGE_PAGE)) {
-		switch (usage->hid & HID_USAGE) {
 		set_bit(EV_REP, hi->input->evbit);
+		switch (usage->hid & HID_USAGE) {
 		/* Only for UK keyboard */
 		/* key found */
 		case 0x32: samsung_kbd_mouse_map_key_clear(KEY_KBDILLUMTOGGLE); break;
@@ -271,8 +271,8 @@ static int samsung_universal_kbd_input_mapping(struct hid_device *hdev,
 		usage->hid & HID_USAGE);
 
 	if (HID_UP_KEYBOARD == (usage->hid & HID_USAGE_PAGE)) {
-		switch (usage->hid & HID_USAGE) {
 		set_bit(EV_REP, hi->input->evbit);
+		switch (usage->hid & HID_USAGE) {
 		/* Only for UK keyboard */
 		/* key found */
 		case 0x32: samsung_kbd_mouse_map_key_clear(KEY_KBDILLUMTOGGLE); break;
